5ed0a65c7fa3e205c7a61d1862f1d712e8794d1c
joelreeves <jmrmb80@gmail.com>
2021-03-15 10:22:20 -0400

    Day 1 lessons

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+// 3/14/21
+
+// Variables
+var joel = "Joel Reeves"
+var joelAge = 40
+
+var jeanette = "Jeanette Reeves"
+var jeanetteAge = 41
+
+var currentYear = 2021
+
+
+// Multi-line strings
+var longSentance = """
+This is an
+example of a
+long sentance.
+"""
+
+
+// String Interpolation
+print("\(joel) is \(joelAge) years old")
+print("\(jeanette) is \(jeanetteAge) years old")
+
+
+// Constants
+let isMarriedToJeanetteReeves = true
+
+let pi = 3.141592653589793238
+
+let favoriteMovie = "Lord of the Rings"
+print("My favorite movie is \(favoriteMovie)")
+
+let vacation = "Trip to Hawaii"
+
+
+// Type Annotation
+let weight: Int = 220
+let myBabe: String = "Jeanette Reeves"
+let weddingDate: String = "02/11/2020"
+
+print("Joel is married to Jeanette(T/F): \(isMarriedToJeanetteReeves). Our wedding date is \(weddingDate)")
+
